825eab32ddfc3d358566e3e5088c3017644ff451
Lowell <lowell.mower@gmail.com>
2018-12-04 08:24:59 -0500

    a run through generics

---
+++ rbe-examples/generics/README.md
@@ -0,0 +1,50 @@
+### Generics
+
+### Concepts
+- Generic parameters
+- 
+
+### Notes
+Generally, a generic type in Rust is represented as `<T>` but can be identified
+as such with angle brackets and camel casing, `<GenericType>` is an example of
+another valid generic. An example of a generic function taking a generic parameter
+```rust
+// reads as, a generic function foo takes an argument of any type T
+fn foo<T>(arg: T) { ... }
+```
+
+Implementations can also handle generics but require some syntactic hurdles to
+execute, they don't read terribly well but one can see a world where they would
+be extraordinarily useful
+```rust
+// impl of GenVal for a generic type `T`
+impl <T> GenVal<T> {
+    fn value(&self) -> &T { &self.gen_val }
+}
+```
+
+Bounds on generics force the implementation of some specific trait, thus placing
+boundaries on what can and cannot respond, making things slightly less 'generic'
+```rust
+// The generic `T` must implement `Debug`. Regardless
+// of the type, this will work properly.
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+// `T` must implement `HasArea`. Any function which meets
+// the bound can access `HasArea`'s function `area`.
+fn area<T: HasArea>(t: &T) -> f64 { t.area() }
+```
+
+Multiple bounds can be placed on a function or trait with the `+` operator like
+```rust
+use std::fmt::{Debug, Display};
+
+fn compare_prints<T: Debug + Display>(t: &T) {
+    println!("Debug: `{:?}`", t);
+    println!("Display: `{}`", t);
+}
+
+// etc ...
+```
---
+++ rbe-examples/generics/bounds.rs
@@ -0,0 +1,58 @@
+// A trait which implements the print marker: `{:?}`.
+use std::fmt::{Debug, Display};
+
+fn compare_prints<T: Debug + Display>(t: &T) {
+    println!("Debug: `{:?}`", t);
+    println!("Display: `{}`", t);
+}
+
+fn compare_types<T: Debug, U: Debug>(t: &T, u: &U) {
+    println!("t: `{:?}", t);
+    println!("u: `{:?}", u);
+}
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 { self.length * self.height }
+}
+
+#[derive(Debug)]
+struct Rectangle { length: f64, height: f64 }
+#[allow(dead_code)]
+struct Triangle  { length: f64, height: f64 }
+
+// The generic `T` must implement `Debug`. Regardless
+// of the type, this will work properly.
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+// `T` must implement `HasArea`. Any function which meets
+// the bound can access `HasArea`'s function `area`.
+fn area<T: HasArea>(t: &T) -> f64 { t.area() }
+
+fn main() {
+    let rectangle = Rectangle { length: 3.0, height: 4.0 };
+    let _triangle = Triangle  { length: 3.0, height: 4.0 };
+
+    print_debug(&rectangle);
+    println!("Area: {}", area(&rectangle));
+
+    //print_debug(&_triangle);
+    //println!("Area: {}", area(&_triangle));
+    // ^ TODO: Try uncommenting these.
+    // | Error: Does not implement either `Debug` or `HasArea`. 
+
+    let string = "words";
+    let array = [1, 2, 3];
+    let vec = vec![1, 2, 3];
+
+    compare_prints(&string);
+    //compare_prints(&array);
+    // TODO ^ Try uncommenting this.
+
+    compare_types(&array, &vec);
+}
